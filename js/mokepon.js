bcb9f3e611e23b7c6ef2a083499fe1bc0ff16a57
Michael Aguas <aguasbonillamichael@gmail.com>
2023-03-03 20:20:34 -0500

    random pet enemy

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,3 +1,7 @@
+function aleatorio(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function gameStarts() {
     let petSelection = document.getElementById("select-pet"); // Lee el html para obtener un elemento por ID
     petSelection.addEventListener("click", selectionPet);     // Agrega los eventos que ejecutan las acciones programadas    
@@ -7,6 +11,7 @@ function gameStarts() {
 function selectionPet() {
 
     const $ = selector => document.getElementById(selector); 
+    const play = 1; 
 
     let checkedHipodoge = $("hipodoge"); 
     let checkedCapipepo = $("capipepo"); 
@@ -18,30 +23,55 @@ function selectionPet() {
 
     if (checkedHipodoge.checked) {
         playerPet.innerHTML = "Hipodoge";
-        alert("Elegiste a Hipodoge");
+        alert("You chose Hipodoge");
 
     } else if (checkedCapipepo.checked) {
         playerPet.innerHTML = "Capipepo";
-        alert("Elegiste a Capipepo");
+        alert("You chose Capipepo");
 
     } else if (checkedRatigueya.checked) {
         playerPet.innerHTML = "Ratigueya";
-        alert("Elegiste a Ratigueya");
+        alert("You chose Ratigueya");
 
     } else if (checkedLangostelvis.checked) {
         playerPet.innerHTML = "Langostelvis";
-        alert("Elegiste a Langostelvis");
+        alert("You chose Langostelvis");
 
     } else if (checkedTucapalma.checked) {
         playerPet.innerHTML = "Tucapalma";
-        alert("Elegiste a Tucapalma");
+        alert("You chose Tucapalma");
 
     } else if (checkedPydos.checked) {
         playerPet.innerHTML = "Pydos";
-        alert("Elegiste a Pydos");
+        alert("You chose Pydos");
         
     } else {
-        alert("No has elegido mascota");
+        alert("You haven´t chosen a pet");
+        play = 0;
+    }
+
+    if (play == 1) { 
+        petEnemy(); 
+    }
+}
+
+function petEnemy () {
+    
+    let randomPet = aleatorio(1,6);
+    let enemyPet = document.getElementById("enemy-pet");
+
+    if (randomPet == 1) {
+        enemyPet.innerHTML = "Hipodoge"; 
+    } else if (randomPet == 2) {
+        enemyPet.innerHTML = "Capipepo"; 
+    } else if (randomPet == 3) {
+        enemyPet.innerHTML = "Ratigueya";
+    } else if (randomPet == 4) {
+        enemyPet.innerHTML = "Langostevil";
+    } else if (randomPet == 5) {
+        enemyPet.innerHTML = "Tucapalma";
+    } else {
+        enemyPet.innerHTML= "Pydos";
     }
 }
 
